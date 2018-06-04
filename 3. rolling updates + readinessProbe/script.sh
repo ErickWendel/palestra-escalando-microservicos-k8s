8bbb48dc2973dbf5d1b0c08260c00e79a4e5fc9d
Erick Wendel <erick.workspace@gmail.com>
2018-06-04 09:04:23 -0300

    speaking
    Signed-off-by: Erick Wendel <erick.workspace@gmail.com>

--- 3. rolling updates + readinessProbe/script.sh
+++ 3. rolling updates + readinessProbe/script.sh
@@ -8,4 +8,5 @@ kubectl rollout status deployment api-heroes
 kubectl rollout undo deployment api-heroes --to-revision=1 
 # kubectl set image deployment api-heroes api-heroes=erickwendel/nodejs-with-mongodb-example:v2 --record
 
-siege -c10 -t60S  http://23.100.22.134:4000/heroes
+HOST=$(kubectl get svc | grep api-heroes | awk '{print $4}')
+siege -c10 -t60S  http://$HOST:4000/heroes
--- 6. autoscaling/script.sh
+++ 6. autoscaling/script.sh
@@ -3,7 +3,7 @@ kubectl scale deployment api-heroes --replicas 20
 
 kubectl autoscale deployment api-heroes --cpu-percent=15 --max=20 --min=1
 kubectl get hpa -w
-kubectl describe hpa php-apache
+kubectl describe hpa api-heroes
 git clone https://github.com/kubernetes-incubator/metrics-server.git
 cd metrics-server/deploy/1.8+
 for D in `ls`
@@ -15,4 +15,4 @@ cd ..
 
 
 HOST=$(kubectl get svc | grep api-heroes | awk '{print $4}')
-siege -c30 -t60S  http://$HOST:4000/heroes
+siege -c60 -t60S  http://$HOST:4000/heroes
